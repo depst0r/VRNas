25ca4b76ebbbd28737f21ddc2a1c1c0264cd6cbf
depstor <depstor90@gmail.com>
2025-06-10 18:02:29 +0300

    ny-accardion-test

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
 import '/src/sass/style.scss';
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
+
+const headers = document.querySelectorAll('.accordion__header');
+
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+
+        const isOpen = content.classList.contains('show');
+
+        document.querySelectorAll('.accordion-content').forEach(c => c.classList.remove('show'));
+        console.log('show')
+
+        if (!isOpen) {
+            content.classList.add('show')
+        } else {
+            content.classList.remove('show')
+        }
+
+    })
+})
