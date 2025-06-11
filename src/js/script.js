3f821c66679fe3fdc0e0c03f18ed75213bdc8788
depstor <depstor90@gmail.com>
2025-06-11 14:17:40 +0300

    accordion-v1

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ import '/src/sass/style.scss';
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
+
 const headers = document.querySelectorAll('.accordion__header');
 
 headers.forEach(header => {
